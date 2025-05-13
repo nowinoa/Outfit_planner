b067ac3244bf1db7c063d5b4fbea79f6c651e3b6
nowinoa <apt.code14@gmail.com>
2025-05-13 19:28:43 +0100

    adding navbar

--- outfit_planner/src/App.js
+++ outfit_planner/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './components/navbar/Navbar.jsx';
 import Home from './pages/home/Home.jsx';
 import Outfits from './pages/outfits/Outfits.jsx';
 import Closet from './pages/closet/Closet.jsx';
@@ -6,9 +7,11 @@ import Closet from './pages/closet/Closet.jsx';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 const App = () => {
   return (
     <Router>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/outfits" element={<Outfits />} />

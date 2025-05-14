607a312e699af23f37c2ecf09e7d7c3eea9a2697
nowinoa <apt.code14@gmail.com>
2025-05-14 09:17:14 +0100

    adding banner

---
+++ outfit_planner/src/components/hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './hero.css';
+
+const Hero = () => {
+  return (
+    <div className='hero'>Banner</div>
+  )
+}
+
+export default Hero
--- outfit_planner/src/pages/home/Home.jsx
+++ outfit_planner/src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './home.css';
+import Hero from '../../components/hero/Hero.jsx';
+
 
 const Home = () => {
   return (
-    <div className='home'>homeeeeee</div>
+    
+    <Hero></Hero>
+    // banner
+    // inputs
+    // result pannel
+    //result car (button save outfit or generate a different one)
   )
 }
 

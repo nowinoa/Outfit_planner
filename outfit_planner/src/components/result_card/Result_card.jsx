61b2556e90a2bd6f3e099def9ab80d6b8c331af4
nowinoa <apt.code14@gmail.com>
2025-05-15 08:07:47 +0100

    blurring unloaded pictures

--- outfit_planner/src/components/result_card/Result_card.jsx
+++ outfit_planner/src/components/result_card/Result_card.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './result_card.css';
 import dummyOutfit from '../../assets/deletion/dummy_outfit.png';
 
-const Result_card = () => {
+const Result_card = ({ loading }) => {
   return (
-    <div className="card">
+    <div className={`card ${loading ? 'loading' : ''}`}>
       <div className="card-inner">
         <img src={dummyOutfit} alt="Outfit" className="card-image" />
       </div>
--- outfit_planner/src/pages/home/Home.jsx
+++ outfit_planner/src/pages/home/Home.jsx
@@ -13,8 +13,9 @@ const Home = () => {
         <h2>Recommended Outfits</h2>
         <div className='result-cards'>
           <Card />
-          <Card />
-          <Card />
+          <Card loading={true} />
+          <Card loading={true} />
+
         </div>
         <button className='reset-btn'>Re-style</button>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et doloribus modi corrupti commodi in distinctio impedit, dolores reprehenderit porro quisquam asperiores inventore quam nostrum excepturi libero assumenda dignissimos minima. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tenetur sint optio pariatur similique, molestias obcaecati, reiciendis voluptatibus totam autem impedit. Facilis, nisi! Nesciunt alias aliquid enim facilis tempore earum.</p>

c6ddbfb7adf55a06911b55b77ba14f2105816da0
Nurlyza Isyira Aliyah Lubis <aliyah@als-macbook-pro.local>
2025-05-18 11:17:28 +0700

    added responsiveness to user_input and buttons

--- outfit_planner/src/components/user_input/User_input.jsx
+++ outfit_planner/src/components/user_input/User_input.jsx
@@ -8,7 +8,14 @@ const Userinput = () => {
       <input type="text" />
 
       <p>What's the dress code?</p>
-      <input type="text" />
+      <select className="dropdown">
+        <option value="">Select...</option>
+        <option value="casual">Casual</option>
+        <option value="smart-casual">Smart Casual</option>
+        <option value="business">Business</option>
+        <option value="formal">Formal</option>
+        <option value="party">Party</option>
+      </select>
 
       <p>What's your style vibe</p>
       <div className="style-buttons">
